97012d6132110795b52d3fa0f7ccc90276ebd0ea
mohamed khaja <mohammedkhaja632@gmail.com>
2024-11-24 14:51:23 +0530

    asscending order program is added

---
+++ src/ArraysProgram/ArraysAscendingOrder.java
@@ -0,0 +1,20 @@
+package ArraysProgram;
+
+public class ArraysAscendingOrder {
+
+	public static void main(String[] args) {
+	  int a[]= {6,3,7,4,0,1,2,5};
+	  for(int i=0;i<a.length;i++) {
+		  for(int j=i+1;j<a.length;j++) {
+			  if(a[i]>a[j]) {
+				  int temp=a[i];
+				  a[i]=a[j];
+				  a[j]=temp;
+				  
+			  }
+		  }
+		  System.out.print(a[i]);
+	  }
+	}
+
+}
